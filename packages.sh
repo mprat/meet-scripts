1ef51b8f9763542a12e0bb9e40977208b7be19d5
Michele Pratusevich <michele.pratusevich@gmail.com>
2014-07-31 18:11:34 +0300

    packages script

---
+++ packages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo apt-get install git \
+chromium-browser \
+gimp \
+mysql-client \
+unrar \
+vim \
+python-pip \
+network-manager \
+recordmydesktop \
+gtk-recordmydesktop \
+build-essential \
+python-setuptools \
+python-dev
+sudo pip install virtualenv \
+virtualenvwrapper \ 
+Django==1.6.5
+wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3059_amd64.deb
+sudo dpkg -i sublime-text_build-3059_amd64.deb
